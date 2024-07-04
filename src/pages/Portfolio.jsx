d360ac994237a9a5700ed1e6286f8cf5f4f05fcf
kasimkkn <kasikkn15@gmail.com>
2024-07-04 11:16:04 +0530

    new project added

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,4 +1,5 @@
 import img1 from '../images/img-1.png'
+import pos from '../images/pos.png'
 import img2 from '../images/snap.png'
 import img3 from '../images/lens.png'
 import img4 from '../images/educa.png'
@@ -13,6 +14,14 @@ const Portfolio = () => {
     <div className="box-container">
 
         <div className="box">
+            <img src={pos} alt="pos"></img>
+            <div className="content">
+                <h3>A Resturant Billing Dekstop App</h3>
+                <p>Billing , Inventory , Reports , POS</p>
+                <a href="https://github.com/Kasimkkn/develpoment-pos" target='_blank'>live</a>
+            </div>
+        </div>
+        <div className="box">
             <img src={img1} alt=""></img>
             <div className="content">
                 <h3>Luxify Shoes Store</h3>
@@ -26,7 +35,7 @@ const Portfolio = () => {
             <div className="content">
                 <h3>SnapGram</h3>
                 <p>A Social Media Web App</p>
-                <a href="https://github.com/Kasimkkn/social-media" target='_blank'>link</a>
+                <a href="https://github.com/Kasimkkn/social-media" target='_blank'>code</a>
             </div>
         </div>
        
@@ -35,7 +44,7 @@ const Portfolio = () => {
             <div className="content">
                 <h3>D-Studio</h3>
                 <p>A Web App For Digitally crafted brand experiences</p>
-                <a href="https://dstudio2.netlify.app" target='_blank'>link</a>
+                <a href="https://dstudio2.netlify.app" target='_blank'>code</a>
             </div>
         </div>
         <div className="box">
@@ -43,7 +52,7 @@ const Portfolio = () => {
             <div className="content">
                 <h3>Miranda-News</h3>
                 <p>A News Paper Web App Frontend</p>
-                <a href="https://github.com/Kasimkkn/eyescare" target='_blank'>link</a>
+                <a href="https://github.com/Kasimkkn/Miranda" target='_blank'>code</a>
             </div>
         </div>
         <div className="box">
@@ -51,7 +60,7 @@ const Portfolio = () => {
             <div className="content">
                 <h3>Eyescare</h3>
                 <p>A Web App Frontend For Selling Eyeglasses</p>
-                <a href="https://github.com/Kasimkkn/eyescare" target='_blank'>link</a>
+                <a href="https://github.com/Kasimkkn/eyescare" target='_blank'>code</a>
             </div>
         </div>
 
@@ -60,7 +69,7 @@ const Portfolio = () => {
             <div className="content">
                 <h3>Educa</h3>
                 <p>A Web App Frontend For Education</p>
-                <a href="https://github.com/Kasimkkn/education" target='_blank'>link</a>
+                <a href="https://github.com/Kasimkkn/education" target='_blank'>code</a>
             </div>
         </div>
     </div>
