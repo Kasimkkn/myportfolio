da93b8cd43d706f4e1b31d3c6692bf273f0e8d42
kasimkkn <kasikkn15@gmail.com>
2024-07-24 11:08:51 +0530

    new rsume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import EducationSection from "../components/EducationSection";
 import SkillsSection from "../components/SkillSection";
 
